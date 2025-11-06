33ac1cea819bd81b9150d149a3187fccadcdd5e2
Renat0Espinoza <renato.espinoza2201@alumnos.ubiobio.cl>
2025-11-06 19:41:41 -0300

    implementacion clase Paciente, LoginRequest, UserDetailsServiceImpl y UserDetailsImpl

--- Desktop/proyecto-software/src/main/java/com/softwareubb/proyecto_software/model/Paciente.java
+++ Desktop/proyecto-software/src/main/java/com/softwareubb/proyecto_software/model/Paciente.java
@@ -1,5 +1,49 @@
 package com.softwareubb.proyecto_software.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pacientes")
 public class Paciente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String rut;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    private String apellido;
+    
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getRut() {
+        return rut;
+    }
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 }
--- Desktop/proyecto-software/src/main/java/com/softwareubb/proyecto_software/service/UserDetailsServiceImpl.java
+++ Desktop/proyecto-software/src/main/java/com/softwareubb/proyecto_software/service/UserDetailsServiceImpl.java
@@ -1,5 +1,25 @@
 package com.softwareubb.proyecto_software.service;
 
-public class UserDetailsServiceImpl {
+import com.softwareubb.proyecto_software.model.User;
+import com.softwareubb.proyecto_software.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository; //
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username) //
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+        return UserDetailsImpl.build(user);
+    }
 }
