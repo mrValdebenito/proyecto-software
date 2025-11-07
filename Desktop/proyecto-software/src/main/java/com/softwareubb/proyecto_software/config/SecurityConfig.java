aeebb60fb055ed3dc74d69e0b2cb17b7d040fde9
Renat0Espinoza <renato.espinoza2201@alumnos.ubiobio.cl>
2025-11-07 03:16:49 -0300

    Organizacion de clases

--- Desktop/proyecto-software/src/main/java/com/softwareubb/proyecto_software/config/SecurityConfig.java
+++ Desktop/proyecto-software/src/main/java/com/softwareubb/proyecto_software/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.softwareubb.proyecto_software.config;
 
+import com.softwareubb.proyecto_software.security.jwt.AuthEntryPointJwt;
+import com.softwareubb.proyecto_software.security.jwt.AuthTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,29 +11,40 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated());
-        return http.build();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); //HU 16: El cliente necesita que la información se mantenga privada y segura
+        return new BCryptPasswordEncoder(); 
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable).exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler)).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll().requestMatchers("/error").permitAll().anyRequest().authenticated());
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
--- Desktop/proyecto-software/src/main/java/com/softwareubb/proyecto_software/payload/request/JwtRequest.java
+++ Desktop/proyecto-software/src/main/java/com/softwareubb/proyecto_software/payload/request/JwtRequest.java
@@ -19,35 +19,20 @@ public class JwtRequest {
     public String getToken() {
         return token;
     }
-    public void setToken(String token) {
-        this.token = token;
-    }
 
     public String getType() {
         return type;
-    }
-    public void setType(String type) {
-        this.type = type;
     }
 
     public Long getId() {
         return id;
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getUsername() {
         return username;
     }
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public List<String> getRoles() {
         return roles;
-    }
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
     }
 }
