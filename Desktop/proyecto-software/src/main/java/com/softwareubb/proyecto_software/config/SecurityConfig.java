7fc3440619a6d171628725bf86eda6a941935ffa
Renat0Espinoza <renato.espinoza2201@alumnos.ubiobio.cl>
2025-11-06 22:36:55 -0300

    Modificacion SecurityConfig

--- Desktop/proyecto-software/src/main/java/com/softwareubb/proyecto_software/config/SecurityConfig.java
+++ Desktop/proyecto-software/src/main/java/com/softwareubb/proyecto_software/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.softwareubb.proyecto_software.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,24 +12,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true) // Habilita @PreAuthorize (HU #13)
-public class SecurityConfig {
 
+public class SecurityConfig {
+@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable) // Deshabilita CSRF para API REST
+            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll() // Rutas de login/registro permitidas
-                .anyRequest().authenticated() // Cualquier otra ruta requiere autenticación (HU #6)
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
             );
         return http.build();
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // Implementa encriptación de contraseñas
+        return new BCryptPasswordEncoder();
     }
 }
