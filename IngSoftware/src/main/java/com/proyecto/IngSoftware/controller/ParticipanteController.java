537834bedd5daccbee978392a118344cc197386c
Lucas Varas <lucas.varas2101@alumnos.ubiobio.cl>
2025-12-19 05:06:42 -0300

    Corrige error de compilación: agrega import faltante para Sort

--- IngSoftware/src/main/java/com/proyecto/IngSoftware/controller/ParticipanteController.java
+++ IngSoftware/src/main/java/com/proyecto/IngSoftware/controller/ParticipanteController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.data.domain.Sort;
 
 import java.io.IOException; 
 import java.util.List;
